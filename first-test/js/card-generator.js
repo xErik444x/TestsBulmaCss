b263554ddb4f09cd47b092a7318b9119190d1788
erik Schwerdt <erikschwerdt18@gmail.com>
2021-02-16 19:51:05 -0300

    hover to cards

--- first-test/js/card-generator.js
+++ first-test/js/card-generator.js
@@ -30,3 +30,14 @@ function generarCard(title, subtitle, imgUrl) {
 cardsMainProducts.innerHTML += generarCard("OFF!", "$1500", "https://off.com.ph/-/media/images/off/ph/products-en/products-landing/landing/off_overtime_product_collections_large_2x.jpg?la=en-ph");
 cardsMainProducts.innerHTML += generarCard("Pingu (Bateria incluida)", "$500", "https://cloud10.todocoleccion.online/figuras-goma-pvc/tc/2015/02/08/05/47630802.jpg");
 cardsMainProducts.innerHTML += generarCard("Pinguino", "$75500", "https://molaserfriki.com/wp-content/uploads/2020/09/figura-pinguino-dc-comics.jpg");
+
+cardsMainProducts.parentElement.innerHTML += `
+<div class="columns cards-products">
+                    
+
+</div>
+`;
+let cardsMainProducts2 = document.querySelectorAll(".cards-products")[1];
+cardsMainProducts2.innerHTML += generarCard("OFF!", "$1500", "https://off.com.ph/-/media/images/off/ph/products-en/products-landing/landing/off_overtime_product_collections_large_2x.jpg?la=en-ph");
+cardsMainProducts2.innerHTML += generarCard("Pingu (Bateria incluida)", "$500", "https://cloud10.todocoleccion.online/figuras-goma-pvc/tc/2015/02/08/05/47630802.jpg");
+cardsMainProducts2.innerHTML += generarCard("Pinguino", "$75500", "https://molaserfriki.com/wp-content/uploads/2020/09/figura-pinguino-dc-comics.jpg");
